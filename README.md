7038c247b65f2625565fabd858767beb37838a36
darksire7 <akifullahahmed1@gmail.com>
2025-05-06 21:38:34 +0530

    Version 1

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-"# pinterest navigation through arrow keys extension"  
+Navigate pinterest using your arrow keys using this extension.
+
+Update log:
+
+Version 1:
+
+1. Limit of 15-20 pins using arrow keys before having to refresh
+2. wait on the home page for 6-7 seconds before opening any pin.
--- pins.js
+++ pins.js
@@ -1,12 +1,72 @@
-localStorage.clear();
 
-setTimeout(function () {
-    let newPins = Array.from(
-        new Set(
-            Array.from(document.querySelectorAll('a[href*="/pin/"]')).map(a => a.href)
-        )
-    );
+let index = Number(localStorage.getItem("index")) || 0;
 
-    console.log(newPins)
+if (window.location.href.includes("/pin/")) {
+    //be silent
 
-}, 6000);
+}
+else {
+    localStorage.clear();
+
+    setTimeout(function () {
+        let newPins = Array.from(
+            new Set(
+                Array.from(document.querySelectorAll('a[href*="/pin/"]')).map(a => a.href)
+            )
+        );
+
+        localStorage.setItem("pins", JSON.stringify(newPins));
+
+
+
+    }, 6000);
+
+
+    localStorage.setItem("index", "0")
+
+}
+
+window.navigation.addEventListener("navigate", (event) => {
+    setTimeout(function () {
+        let index = Number(localStorage.getItem("index"))
+
+        if (index == 0) {
+            window.location.reload();
+
+        }
+    }, 2000)
+
+});
+
+
+document.addEventListener('keydown', function (event) {
+
+
+
+    let pins = JSON.parse(localStorage.pins);
+    if (event.key === "ArrowRight") {
+
+        index++;
+
+        localStorage.setItem("index", index)
+        window.open(pins[index], "_self")
+        if (index == pins.length) {
+            alert("reached end of list, go back to home pages and refresh")
+            index = 0;
+            localStorage.setItem("index", "0")
+        }
+    }
+    else if (event.key === "ArrowLeft") {
+
+        index--;
+        localStorage.setItem("index", index)
+        window.open(pins[index], "_self")
+
+        if (index < 0) {
+            alert("Go forward, nothing to see back here")
+        }
+    }
+})
+
+
+
